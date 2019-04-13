dbddd66e8eeeb31a135194a441408155b6d9cca6
ghehd231 <ghehd231@naver.com>
2019-04-13 16:35:07 +0900

    singup

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,28 @@
+const User = require('../models/usersModel');
+
 module.exports = {
     signUp: async (req, res, next) => {
-        console.log("usersController signUp()");
+        //console.log("usersController signUp()");
+
+        const {email, password} = req.value.body;
+
+        const foundUser = await User.findOne({email});
+        if(foundUser){
+            return res.status(403).json({error: 'Email is already in use'});
+        }
+    
+        //create a new usre
+        const newUser = new User({email, password});
+        await newUser.save();
+
+        //response with token
+        res.status(200).json({user: 'created'});
     },
+    
     signIn: async (req, res, next) => {
         console.log("usersController signIn()");
     },
+
     secret: async (req, res, next) => {
         console.log("usersController secret()");
     }
