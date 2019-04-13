5ec467e61cb87e74719e4036aec912c248432b91
ghehd231 <ghehd231@naver.com>
2019-04-13 15:44:30 +0900

    router, controller setting

--- App.js
+++ App.js
@@ -2,10 +2,15 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const usersRouter = require('./routers/usersRouter');
+
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json()); //body 를 json으로 뿌려줌
+
+app.use('/users',usersRouter);
+
 
 const PORT = process.env.PORT || 3000;
 
---
+++ controllers/usersController.js
@@ -0,0 +1,12 @@
+module.exports = {
+    signUp: async (req, res, next) => {
+        console.log("usersController signUp()");
+    },
+    signIn: async (req, res, next) => {
+        console.log("usersController signIn()");
+    },
+    secret: async (req, res, next) => {
+        console.log("usersController secret()");
+    }
+
+}
