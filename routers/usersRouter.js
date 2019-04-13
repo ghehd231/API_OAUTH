a551aaafb91a6381be68543d217f135cb4d30931
ghehd231 <ghehd231@naver.com>
2019-04-13 15:59:22 +0900

    Joi use

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = require('express-promise-router')();
 const usersController = require('../controllers/usersController');
 
+const {validateBody, schemas} = require('../helpers/routeHelpers');
+
 router.route('/signup')
-    .post(usersController.signUp);
+    //컨트롤러 이동 전에 검증 후 이동
+    .post(validateBody(schemas.authSchema), usersController.signUp);
 router.route('/signin')
     .post(usersController.signIn);
+
 router.route('/secret')
     .get(usersController.secret);
 
